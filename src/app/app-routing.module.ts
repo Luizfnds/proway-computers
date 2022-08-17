9521b3dddbdd9c4973e3fb50e70af1a2835e4089
Luizfnds <luizeduardofnds@gmail.com>
2022-08-16 21:37:40 -0300

    carrinho

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { NaoEncontradaComponent } from './nao-encontrada/nao-encontrada.componen
 const routes: Routes = [
   { path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
   { path: '', redirectTo: 'produtoes', pathMatch: 'full'},
+  { path: 'carrinho', loadChildren: () => import('./carrinho/carrinho.module').then(m => m.CarrinhoModule) },
   { path: '**', component: NaoEncontradaComponent}
 ];
 
